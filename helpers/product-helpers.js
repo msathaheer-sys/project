659b2fbfeb98daf3dee7438e4da8ca9c67cc5bcc
ABOOTHAHEER <msathaheer@gmail.com>
2026-02-26 22:42:24 +0530

    Web Development Class 21 Edit product

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,6 +1,7 @@
 var db = require('../config/connections');
 var collection = require('../config/collections');
 const { ObjectId } = require('mongodb');
+const { response } = require('../app');
 
 module.exports = {
     addProduct: (product, callback) => {
@@ -18,10 +19,34 @@ module.exports = {
         return new Promise((resolve, reject) => {
             db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({ _id: new ObjectId(prodId) }).then((response) => {
                 resolve(response);
-            })
-                .catch((err) => {
-                    reject(err);
-                });
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    },
+    getProductDetails: (prodId) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({ _id: new ObjectId(prodId) }).then((product) => {
+                resolve(product);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    },
+    updateProduct: (prodId, proDetails) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({ _id: new ObjectId(prodId) }, {
+                $set: {
+                    Name: proDetails.Name,
+                    Description: proDetails.Description,
+                    Price: proDetails.Price,
+                    Category: proDetails.Category
+                }
+            }).then((response) => {
+                resolve();
+            }).catch((err) => {
+                reject(err);
+            });
         });
     }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -34,12 +34,29 @@ router.get('/delete-product/:id', (req, res) => {
   console.log(prodId);
   productHelpers.deleteProduct(prodId).then((response) => {
     res.redirect('/admin/');
-  })
-})
+  });
+});
 // router.get('/delete-product/',(req,res)=>{
 // let proId=req.query.id;
 // console.log(proId);
 // console.log(req.query.name);
 // })
+
+router.get('/edit-product/:id', async (req, res) => {
+  let product = await productHelpers.getProductDetails(req.params.id);
+  res.render('admin/edit-product', { product });
+});
+
+router.post('/edit-product/:id', (req, res) => {
+  let id = req.params.id;
+  productHelpers.updateProduct(req.params.id, req.body).then(() => {
+    res.redirect('/admin');
+    if (req.files.Image) {
+      let image = req.files.Image
+      image.mv('./public/product-images/' + id + '.jpg');
+    }
+  });
+});
+
 
 module.exports = router;
